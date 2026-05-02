e937be6680238759394cf9aba3a99ceb198debbd
Vasyl Krychfalushii <krychvas03@gmail.com>
2026-05-02 23:14:18 +0200

    feat: solve brackets task with stack and destructuring

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const stack = [];
+  const openBrackets = [];
+  const bracketsPairs = {};
+
+  // Використовуємо деструктуризацію [open, close]
+  bracketsConfig.forEach(([open, close]) => {
+    bracketsPairs[close] = open;
+    openBrackets.push(open);
+  });
+
+  for (let i = 0; i < str.length; i += 1) {
+    const currentSymbol = str[i];
+    const topElement = stack[stack.length - 1];
+
+    if (bracketsPairs[currentSymbol] === currentSymbol) {
+      if (topElement === currentSymbol) {
+        stack.pop();
+      } else {
+        stack.push(currentSymbol);
+      }
+    } else if (openBrackets.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      if (bracketsPairs[currentSymbol] === topElement) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 };
